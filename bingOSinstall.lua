0fd88b983abb48c6989ce0395aa3bf2c0de61fa5
DorianH2011 <103533452+DorianH2011@users.noreply.github.com>
2024-06-02 12:37:19 -0500

    Update bingOSinstall.lua

--- bingOSinstall.lua
+++ bingOSinstall.lua
@@ -1,26 +1,26 @@
-local function a(url, path)
-    local b = http.get(url)
-    if b then
-        local c = fs.open(path, "w")
-        c.write(b.readAll())
-        c.close()
-        b.close()
+local function a(b, c)
+    local d = http.get(b)
+    if d then
+        local e = fs.open(c, "w")
+        e.write(d.readAll())
+        e.close()
+        d.close()
         return true
     else
         return false
     end
 end
 
-local function b(key)
-    local c = "BOS".."7151"
-    return key == c
+local function f(g)
+    local h = "BOS".."7151"
+    return g == h
 end
 
-local function d()
+local function i()
     print("Please enter the product key to install:")
-    local e = read("*")
+    local j = read("*")
 
-    if not b(e) then
+    if not f(j) then
         print("Invalid product key! Installation aborted.")
         return
     end
@@ -30,9 +30,9 @@ local function d()
     end
     
     print("Downloading menu.lua...")
-    local f = string.char(104, 116, 116, 112, 115, 58, 47, 47, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109, 47, 68, 111, 114, 105, 97, 110, 72, 50, 48, 49, 49, 47, 66, 105, 110, 103, 79, 83, 47, 114, 97, 119, 47, 109, 97, 105, 110, 47, 109, 101, 110, 117, 46, 108, 117, 97)
-    local g = "/startup/menu.lua"
-    if a(f, g) then
+    local k = string.char(104, 116, 116, 112, 115, 58, 47, 47, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109, 47, 68, 111, 114, 105, 97, 110, 72, 50, 48, 49, 49, 47, 66, 105, 110, 103, 79, 83, 47, 114, 97, 119, 47, 109, 97, 105, 110, 47, 109, 101, 110, 117, 46, 108, 117, 97)
+    local l = "/startup/menu.lua"
+    if a(k, l) then
         print("menu.lua downloaded successfully!")
     else
         print("Failed to download menu.lua")
@@ -40,9 +40,9 @@ local function d()
     end
     
     print("Downloading flopper.lua...")
-    local h = string.char(104, 116, 116, 112, 115, 58, 47, 47, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109, 47, 68, 111, 114, 105, 97, 110, 72, 50, 48, 49, 49, 47, 66, 105, 110, 103, 79, 83, 47, 114, 97, 119, 47, 109, 97, 105, 110, 47, 102, 108, 111, 112, 112, 101, 114, 46, 108, 117, 97)
-    local i = "/bingapps/flopper.lua"
-    if a(h, i) then
+    local m = string.char(104, 116, 116, 112, 115, 58, 47, 47, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109, 47, 68, 111, 114, 105, 97, 110, 72, 50, 48, 49, 49, 47, 66, 105, 110, 103, 79, 83, 47, 114, 97, 119, 47, 109, 97, 105, 110, 47, 102, 108, 111, 112, 112, 101, 114, 46, 108, 117, 97)
+    local n = "/bingapps/flopper.lua"
+    if a(m, n) then
         print("flopper.lua downloaded successfully!")
     else
         print("Failed to download flopper.lua")
@@ -55,4 +55,4 @@ local function d()
     os.reboot()
 end
 
-d()
+i()
