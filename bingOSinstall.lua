f681ed762609bde754ce7f54e2405d21c9f19847
DorianH2011 <103533452+DorianH2011@users.noreply.github.com>
2024-06-04 14:59:59 -0500

    Update bingOSinstall.lua

--- bingOSinstall.lua
+++ bingOSinstall.lua
@@ -29,9 +29,18 @@ local function i()
         fs.makeDir("/bingapps")
     end
     
+    local l = "/startup/menu.lua"
+    if fs.exists(l) then
+        fs.delete(l)
+    end
+
+    local n = "/bingapps/flopper.lua"
+    if fs.exists(n) then
+        fs.delete(n)
+    end
+    
     print("Downloading menu.lua...")
     local k = string.char(104, 116, 116, 112, 115, 58, 47, 47, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109, 47, 68, 111, 114, 105, 97, 110, 72, 50, 48, 49, 49, 47, 66, 105, 110, 103, 79, 83, 47, 114, 97, 119, 47, 109, 97, 105, 110, 47, 109, 101, 110, 117, 46, 108, 117, 97)
-    local l = "/startup/menu.lua"
     if a(k, l) then
         print("menu.lua downloaded successfully!")
     else
@@ -41,7 +50,6 @@ local function i()
     
     print("Downloading flopper.lua...")
     local m = string.char(104, 116, 116, 112, 115, 58, 47, 47, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109, 47, 68, 111, 114, 105, 97, 110, 72, 50, 48, 49, 49, 47, 66, 105, 110, 103, 79, 83, 47, 114, 97, 119, 47, 109, 97, 105, 110, 47, 102, 108, 111, 112, 112, 101, 114, 46, 108, 117, 97)
-    local n = "/bingapps/flopper.lua"
     if a(m, n) then
         print("flopper.lua downloaded successfully!")
     else
